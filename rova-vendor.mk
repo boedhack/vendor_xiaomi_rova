049bededef03a75b18a4a5409ac61994aaaede09
zqm64 <zqm64@outlook.com>
2020-11-06 15:07:26 +0800

    rova: rolex has LED IR

--- rova-vendor.mk
+++ rova-vendor.mk
@@ -27,8 +27,8 @@ PRODUCT_COPY_FILES += \
 
 
 PRODUCT_COPY_FILES += \
-    vendor/xiaomi/rova/proprietary/vendor/lib/hw/lirc/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/lirc/consumerir.default.so \
-    vendor/xiaomi/rova/proprietary/vendor/lib64/hw/lirc/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/lirc/consumerir.default.so \
+    vendor/xiaomi/rova/proprietary/vendor/lib/hw/led/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/led/consumerir.default.so \
+    vendor/xiaomi/rova/proprietary/vendor/lib64/hw/led/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/led/consumerir.default.so \
     vendor/xiaomi/rova/proprietary/vendor/lib/hw/spidev/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib/hw/spidev/consumerir.default.so \
     vendor/xiaomi/rova/proprietary/vendor/lib64/hw/spidev/consumerir.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/spidev/consumerir.default.so
 
